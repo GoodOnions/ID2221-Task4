fb023db78f075e0d39a6dcf597d30033f24ab577
Daniele Cipollone <dan.cip00@gmail.com>
2023-10-08 11:44:25 +0200

    Row data parsing - Spark

--- spark/master/spark_data/data_clean.py
+++ spark/master/spark_data/data_clean.py
@@ -1,4 +1,25 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode
+from pyspark.sql import SparkSession
+import json
+from pyspark.sql.functions import from_json, col, get_json_object
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, TimestampType, ArrayType
+
+# Define the schema, replace with what you expect
+response_schema = StructType([
+    StructField("user_id", IntegerType(),True),
+    StructField("items", StringType(),True),
+    StructField("created_at", TimestampType(),True),
+    StructField("updated_at", TimestampType(),True)
+])
+
+item_schema = StructType([
+    StructField("track", StringType(),True),
+    StructField("played_at", StringType(),True),
+    StructField("context", StringType(),True)
+])
+
+
 
 packages = [
     'org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2'
@@ -20,7 +41,23 @@ df = spark\
         .option("startingOffsets", "earliest")\
         .load()
 
-query = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)") \
+
+
+json_df = df.select(explode(from_json(col("value").cast("string"), ArrayType(response_schema))).alias('json')).select('json.*')
+
+json_df = json_df.select('*',explode(from_json(col("items").cast("string"), ArrayType(item_schema))).alias('item')).select('*','item.*')
+
+json_df = json_df.select('user_id','played_at',\
+                         get_json_object(col("track").cast("string"), "$.id").alias("track_id"),\
+                         get_json_object(col("track").cast("string"), "$.name").alias("track_name"),\
+                         get_json_object(col("context").cast("string"), "$.type").alias("context_type"),)
+
+
+query = json_df.selectExpr("CAST(user_id AS STRING)",\
+                           "CAST(played_at AS STRING)",\
+                           "CAST(track_id AS STRING)",\
+                           "CAST(track_name AS STRING)",\
+                           "CAST(context_type AS STRING)")\
     .writeStream \
     .format("console") \
     .start()
