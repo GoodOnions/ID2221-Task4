0a235298528e1767534089cf347df8bd7cbde482
Federico <federicobono99@gmail.com>
2023-10-18 15:20:05 +0200

    Add create script

--- create.sh
+++ create.sh
@@ -1,4 +1,13 @@
-cp ./spotify-connector/.env.example .env
+docker-compose up -d
 
-docker-compose -f compose.yaml -f compose.connector.yaml create
+cd ./spotify-connector
 
+cp .env.example .env
+
+docker-compose up -d
+
+docker exec spotify-connector-laravel.test-1 composer install 
+docker exec spotify-connector-laravel.test-1 php artisan key:generate
+docker exec spotify-connector-laravel.test-1 php artisan migrate
+docker exec spotify-connector-laravel.test-1 npm install
+docker exec spotify-connector-laravel.test-1 npm run build
